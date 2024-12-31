653140f426748944c97281c9141b4beb72f7614a
mikkyy99 <140974247+mikkyy99@users.noreply.github.com>
2024-12-31 14:01:03 +0100

    actor.py

--- examples/actor.py
+++ examples/actor.py
@@ -1,119 +1,89 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.channels.SocketChannel;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
+public class Actor {
+    private String address;
+    private WriteFunction write;
+    private int state;
 
-"""
-This is an example that showcases the bus_monitor function.
+    public Actor(String address, WriteFunction write) {
+        this.address = address;
+        this.write = write;
+        this.state = 0;
+    }
 
-It is a command line application that can be used to toggle the state of a
-single actor (a Light for example).
-"""
+    public void toggle() {
+        int newState = (state == 0) ? 1 : 0;
+        System.out.println("Sending value: " + newState + " to address: " + address);
+        write.write(address, newState);
+    }
 
+    public void send(Telegram telegram) {
+        if (!telegram.getDst().equals(address)) {
+            return;
+        }
+        if (!(telegram.getValue() instanceof Integer)) {
+            return;
+        }
+        int value = (int) telegram.getValue();
+        if (value < 0) {
+            return;
+        }
+        System.out.println("New state of Actor " + address + " is " + value);
+        this.state = value;
+    }
 
-import os
-import signal
-import asyncio
-import argparse
-from knx import connect
+    @Override
+    public String toString() {
+        return "Actor<" + address + ">";
+    }
 
+    public static void main(String[] args) throws IOException {
+        if (args.length != 2) {
+            System.err.println("Usage: java Actor <host> <actor address>");
+            System.exit(1);
+        }
 
-class Actor:
-    """ Represents an actor on the bus system
+        String host = args[0];
+        String actorAddress = args[1];
+        String[] hostParts = host.split(":");
+        String hostname = hostParts[0];
+        int port = (hostParts.length > 1) ? Integer.parseInt(hostParts[1]) : 6720;
 
-    It has a toggle function in order to change the state from 0 (off) to 1
-    (on) and back again.
-    """
+        System.out.println("Creating connection to " + hostname + ":" + port);
 
-    def __init__(self, address, write_func):
-        """ create an actor
+        try (SocketChannel socketChannel = SocketChannel.open(new InetSocketAddress(hostname, port))) {
+            Actor actor = new Actor(actorAddress, new WriteFunction(socketChannel));
+            ScheduledExecutorService executorService = Executors.newScheduledThreadPool(1);
+            Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+                System.out.println("Byte");
+                executorService.shutdown();
+            }));
 
-        :param address: The address of the actor in the bus system.
-                        Something like '0/0/20'
-        :param write_func: A reference to AsyncKnx.write
-        """
+            executorService.scheduleAtFixedRate(actor::toggle, 0, 10, TimeUnit.SECONDS);
+        }
+    }
+}
 
-        self.address = address
-        self.write = write_func
-        self.state = 0
+interface WriteFunction {
+    void write(String address, int value);
+}
 
-    def toggle(self):
-        newstate = 1 if self.state == 0 else 0
-        print('Sending value: {value} to address: {address}'.format(
-            value=newstate,
-            address=self.address))
+class Telegram {
+    private String dst;
+    private Object value;
 
-        self.write(self.address, newstate)
+    // Getters, setters, and other necessary methods
 
-    def send(self, telegram):
-        """ This metod receives telegrams from the bus system.
+    public String getDst() {
+        return dst;
+    }
 
-        It uses the telegram to update the state of the actor.  This way
-        toggle() will always do the right thing, even if the state of the actor
-        has been changed without using toggle (E.g. by using a regular light
-        switch)
-
-        Further down in ``main()`` an instance of this ``Actor`` class is being
-        passed to `AsyncKnx.listen`` as a ``receiver``.
-
-        A ``receiver`` is any object that has a send method with one argument
-        (the telegram).
-        """
-
-        if telegram.dst != self.address:
-            # this telegram is for a different actor
-            return
-        if not isinstance(telegram.value, int):
-            return
-        value = int(telegram.value)
-        if value < 0:
-            # -1 are read requests
-            return
-        print('New state of Actor {address} is {value}'.format(
-            address=self.address,
-            value=value))
-        self.state = value
-
-    def __repr__(self):
-        return 'Actor<{address}>'.format(address=self.address)
-
-
-def parse_host_and_port(host):
-    if ':' in host:
-        return host.split(':')
-    return host, 6720  # default port of EIBD
-
-
-def main():
-    """
-    Monitor telegrams for a specific actor and toggle it's state using kill -USR1
-
-    Usage:
-        python example_actor.py eibd_host[:port] actor_address
-
-    Example:
-        python example_actor.py localhost '0/0/20'
-    """
-    parser = argparse.ArgumentParser()
-    parser.add_argument("host", type=str)
-    parser.add_argument("actor", type=str)
-    args = parser.parse_args()
-
-    host, port = parse_host_and_port(args.host)
-    print('Creating connection to {host}:{port}'.format(host=host, port=port))
-
-    conn = connect(host=host, port=port)
-    actor = Actor(args.actor, conn.write)
-    loop = asyncio.get_event_loop()
-    loop.add_signal_handler(signal.SIGUSR1, actor.toggle)
-    print('Use "kill -USR1 {pid}" to toggle state'.format(pid=os.getpid()))
-    loop.call_soon(lambda: conn.read(actor.address))  # read request for initial state
-    try:
-        loop.run_until_complete(conn.bus_monitor(actor))
-    except KeyboardInterrupt:
-        print('Byte')
-    finally:
-        loop.close()
-
-
-if __name__ == "__main__":
-    main()
+    public Object getValue() {
+        return value;
+    }
+}
