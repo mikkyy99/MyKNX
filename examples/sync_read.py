cd2e5b49182b0a48c90795c5c38646b2ccca52bf
mikkyy99 <140974247+mikkyy99@users.noreply.github.com>
2024-12-31 14:02:28 +0100

    sync_read.py

--- examples/sync_read.py
+++ examples/sync_read.py
@@ -1,11 +1,21 @@
-#!/usr/bin/env python
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.channels.SocketChannel;
 
-from knx import connect
+public class SyncRead {
+    public static void main(String[] args) throws IOException {
+        String host = "nuc";
+        try (SocketChannel socketChannel = SocketChannel.open(new InetSocketAddress(host, 6720))) {
+            // This only sends a request to the bus which "requests" the current state
+            // The response from the bus is sent asynchronously and a bus_monitor/listener
+            // has to be used to retrieve the actual state.
 
-with connect('nuc') as c:
-    # this only sends a request to the bus which "requests" the current state
-    # the response from the bus is sent asynchronous and a bus_monitor/listener
-    # has to be used to retrieve the actual state.
+            // See examples/actor.py for an example which does that.
+            write(socketChannel, "0/0/20");
+        }
+    }
 
-    # see examples/actor.py for an example which does that.
-    c.read('0/0/20')
+    private static void write(SocketChannel socketChannel, String address) {
+        // Implement the write logic to send the request to the bus
+    }
+}
